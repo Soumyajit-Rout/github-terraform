125db371ba83ac06d12559512a82f82786d42a52
Soumyajit-Rout <115661404+Soumyajit-Rout@users.noreply.github.com>
2025-01-09 09:37:21 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,8 @@
   terraform {
  backend "s3" {
- bucket = "idream-terraform-state"
+ bucket = "idream-terraform-state7"
  key = "terraform.tfstate"
- region = "ap-south-1"
+ #region = "ap-south-1"
+region = "eu-west-3"
       }
 }
